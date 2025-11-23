219d3450942ab387b41c4b6987cb93c2e02dff3b
AluilsonFilho5 <aluilsoneduc55@gmail.com>
2025-11-23 20:19:27 -0300

    criando componente Navigation com AppBar do MUI e links de navegação principais

---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,23 @@
+import { AppBar, Toolbar, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#1f80e0" }}>
+      <Toolbar sx={{ padding: "1rem 2rem" }}>
+        <Typography variant="h6" sx={{ flexGrow: 1, fontSize: "1.5rem" }}>
+          Lançamentos 2025
+        </Typography>
+        <Button color="inherit" component={Link} to="/" sx={{ fontSize: "1.1rem" }}>
+          Início
+        </Button>
+        <Button color="inherit" component={Link} to="/filmes" sx={{ fontSize: "1.1rem" }}>
+          Filmes
+        </Button>
+        <Button color="inherit" component={Link} to="/series" sx={{ fontSize: "1.1rem" }}>
+          Séries
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
